8eff730e0dfd000358d4831d1d303022c1e28643
Nitai Seri <Nitai.ns@gmail.com>
2022-11-08 11:44:27 +0200

    finish input react

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -10,12 +10,25 @@ class Exercise1 extends Component {
         }
     }
 
+    alertButton = () => {
+        alert(`You are ${this.state.name} and, ${this.state.age} years old.`)
+    }
+
+    hundleInput = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.id === 'name-input' ? 'name' : 'age';
+        this.setState({
+          [name]: value
+        });
+    }
+
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" value={this.state.name} onChange={this.hundleInput}/>
+                <input id="age-input" value={this.state.age} onChange={this.hundleInput}/>
+                <button onClick={this.alertButton}>Go to Bar</button>
             </div>
         );
     }
--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -10,11 +10,46 @@ constructor() {
     }
 }
 
+alertChoise = () => {
+    alert(`${this.state.name} selected ${this.state.fruit}`)
+}
+
+handleNameChange = (event) => {
+    this.setState({ name: event.target.value });
+}
+
+handleFruitChange = (event) => {
+    this.setState({ fruit: event.target.value }, this.alertChoise);
+}
+
 render() {
+
+    const options = [
+        {
+          label: "Apple",
+          value: "apple",
+        },
+        {
+          label: "Mango",
+          value: "mango",
+        },
+        {
+          label: "Banana",
+          value: "banana",
+        },
+        {
+          label: "Pineapple",
+          value: "pineapple",
+        },
+      ];
+
     return (
         <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
+            <input id="name-input" onChange={this.handleNameChange}/>
+            <select id="select-input" onChange={this.handleFruitChange}>
+                {options.map((option) => (
+                <option value={option.value}>{option.label}</option>))}
+            </select>
         </div>
     );
 }
